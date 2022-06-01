842dd04653fb32faf8330ebc7fb13ca6f989abdd
Sandeep Menon <sandeepmenon@deepen.ai>
2022-06-01 17:21:51 +0530

    directory path and extension in command line argument for image path generation

--- python_utils/image_paths.py
+++ python_utils/image_paths.py
@@ -1,10 +1,16 @@
 import os
+import argparse
 
-directory_path = "/home/menonsandu/Downloads/c1158-nbNrNAmroorTA-output"
-output_file = os.path.join(directory_path, "image_paths.txt")
+# Read directory_path and extension from command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--directory_path', type=str, required=True)
+parser.add_argument('--extension', default="jpg", type=str, required=True)
+args = parser.parse_args()
+
+output_file = os.path.join(args.directory_path, "image_paths.txt")
 # Write the paths to a file
 with open(output_file, "w") as f:
-    for root, dirs, files in os.walk(directory_path):
+    for root, dirs, files in os.walk(args.directory_path):
         for file in files:
-            if file.endswith(".jpg"):
+            if file.endswith(f".{args.extension}"):
                 f.write(os.path.join(root, file) + "\n")        
